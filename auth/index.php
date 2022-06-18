35a4e72dca1f97d66e89beb7b56b5fbd42c63ee4
KsenijaBojchevska <ksenija.bojchevska@gmail.com>
2022-06-18 16:25:47 +0200

    changes added

--- auth/index.php
+++ auth/index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../style.css">
 </head>
 
 <body>
@@ -17,7 +18,7 @@
         <label>Password</label><br />
         <input type="password" name="password" /><br />
         <br />
-        <button>Login</button>
+        <button class="loginBtn">Login</button>
     </form>
 
     <form method="POST" action="register.php">
@@ -27,7 +28,7 @@
         <label>Password</label><br />
         <input type="password" name="password" /><br />
         <br />
-        <button>Register</button>
+        <button class="registerBtn">Register</button>
     </form>
 </body>
 
